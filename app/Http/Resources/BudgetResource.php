eef43c0224e7dff2b920a3693d08f47fa4d30236
Bachtiar Arya Habibie <bachtiarah73@gmail.com>
2026-08-14 20:27:56 +0700

    feat: add budget API resource

---
+++ app/Http/Resources/BudgetResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BudgetResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'amount' => $this->amount,
+            'starts_at' => $this->starts_at?->toDateString(),
+            'ends_at' => $this->ends_at?->toDateString(),
+            'created_at' => $this->created_at?->toIso8601String(),
+            'updated_at' => $this->updated_at?->toIso8601String(),
+            'accounts' => AccountResource::collection($this->whenLoaded('accounts')),
+            'tags' => TagResource::collection($this->whenLoaded('tags')),
+        ];
+    }
+}
