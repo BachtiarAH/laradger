1ebc2ef2a0be8b47ed635116ef8dcaf43fa9d8bf
Bachtiar Arya Habibie <bachtiarah73@gmail.com>
2026-08-14 20:28:29 +0700

    test: add budget factory

---
+++ database/factories/BudgetFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Budget;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Budget>
+ */
+class BudgetFactory extends Factory
+{
+    protected $model = Budget::class;
+
+    public function definition(): array
+    {
+        $startsAt = $this->faker->dateTimeBetween('-2 months', '+1 month');
+
+        return [
+            'user_id' => User::factory(),
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->optional()->sentence(),
+            'amount' => $this->faker->randomFloat(2, 100000, 10000000),
+            'starts_at' => $startsAt->format('Y-m-d'),
+            'ends_at' => (clone $startsAt)->modify('+'.random_int(1, 31).' days')->format('Y-m-d'),
+        ];
+    }
+}
