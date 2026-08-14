907fa730a31837613d14e1247ab507c1b915f9a7
Bachtiar Arya Habibie <bachtiarah73@gmail.com>
2026-08-14 20:28:52 +0700

    feat: expose tag description and budgets in API

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -7,20 +7,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class TagResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
             'name' => $this->name,
             'type' => $this->type,
+            'description' => $this->description,
             'created_at' => $this->created_at?->toIso8601String(),
             'updated_at' => $this->updated_at?->toIso8601String(),
             'journals' => JournalResource::collection($this->whenLoaded('journals')),
+            'budgets' => BudgetResource::collection($this->whenLoaded('budgets')),
         ];
     }
 }
