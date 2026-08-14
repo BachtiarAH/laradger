cb913b9ed535ae9e1368114776a5cc0ead0dd934
Bachtiar Arya Habibie <bachtiarah73@gmail.com>
2026-08-14 20:28:59 +0700

    fix: enable budget factory

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -2,14 +2,17 @@
 
 namespace App\Models;
 
+use Database\Factories\BudgetFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 #[Fillable(['user_id', 'name', 'description', 'amount', 'starts_at', 'ends_at'])]
 class Budget extends Model
 {
-    use HasUuids;
+    /** @use HasFactory<BudgetFactory> */
+    use HasFactory, HasUuids;
 
     public function user()
     {
